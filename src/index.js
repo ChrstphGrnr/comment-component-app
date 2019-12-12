1a876bc7e02d05b2df66c3ae183faf041e014b94
chrstphgrnr <christophgruener@me.com>
2019-12-12 12:50:44 -0700

    add custom approval card

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure we want to do this?
+                </div>
+            </ApprovalCard>
             < ApprovalCard>
                 <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 5:33PM" content={faker.lorem.words()} />
             </ApprovalCard>
-            <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 12:15PM" content={faker.lorem.words()} />
-            <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 7:11AM" content={faker.lorem.words()} />
-            
+            <ApprovalCard>
+                <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 12:15PM" content={faker.lorem.words()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 7:11AM" content={faker.lorem.words()} />
+            </ApprovalCard>
         </div>
     )
 }

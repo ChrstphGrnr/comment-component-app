89307c383f7a8f4e8a777010f5968dd2ea42140d
chrstphgrnr <christophgruener@me.com>
2019-12-12 12:46:54 -0700

    change props of component

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
     return (
         <div className="ui container comments">
-            < ApprovalCard />
-            <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 5:33PM" content={faker.lorem.words()} />
+            < ApprovalCard>
+                <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 5:33PM" content={faker.lorem.words()} />
+            </ApprovalCard>
             <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 12:15PM" content={faker.lorem.words()} />
             <CommentDetail name={faker.name.firstName()} img={faker.image.avatar()} time="Today at 7:11AM" content={faker.lorem.words()} />
             
